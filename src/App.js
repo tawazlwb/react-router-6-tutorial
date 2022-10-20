ed8c7277f6c4f72b049091ac719a2b78808b330d
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-20 11:37:03 +0200

    164. First Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>home page</div>}></Route>
+        <Route path='testing' element={<h2>testing</h2>}></Route>
+        <Route path='about' element={<div>about</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
