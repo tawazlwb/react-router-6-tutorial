ba73a4c4d46b561fd61a21bd396ec112d2d3ccfa
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-20 11:59:26 +0200

    166. Link Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home, About, Products } from './pages'
 
 function App() {
-  console.log(Home)
   return (
     <BrowserRouter>
       <Routes>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+
 export const About = () => {
   return (
     <section className='section'>
       <h2>About</h2>
+      <Link to='/' className='btn'>
+        Home
+      </Link>
     </section>
   )
 }
